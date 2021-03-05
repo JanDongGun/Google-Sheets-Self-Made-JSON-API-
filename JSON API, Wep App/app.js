18954b3d22f1f8b30c3f27f11a323e7103b03f4e
jandonggun <donggontrannguyen@gmail.com>
2021-03-05 14:55:02 +0700

    add row to google sheet

--- JSON API, Wep App/app.js
+++ JSON API, Wep App/app.js
@@ -8,4 +8,20 @@ const testGS = () => {
     });
 };
 
-document.querySelector("button").addEventListener("click", testGS);
+document.querySelector("#click").addEventListener("click", testGS);
+
+const addRow = () => {
+  const url =
+    "https://script.google.com/macros/s/AKfycbwGghfBaXwqYT8H-HZowqh2Sn8MDFJtiCDmbxaxU3zLH7ahzXxROJ1LKZn9A-c7pYyoeA/exec";
+  fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "no-cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow", // manual, *follow, error
+    body: JSON.stringify({ name: "donggon" }), // body data type must match "Content-Type" header
+  });
+};
+document.querySelector("#addRow").addEventListener("click", addRow);
