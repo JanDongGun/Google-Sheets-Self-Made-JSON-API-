5b536e551515ef602de5c886fb0c7d104d098752
jandonggun <donggontrannguyen@gmail.com>
2021-03-09 15:27:18 +0700

    create api google sheet

--- ReadWriteData, WepApp/src/Code.js
+++ ReadWriteData, WepApp/src/Code.js
@@ -1,5 +1,19 @@
 function doGet() {
-  const response = [{ status: "cool!" }];
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const ws = ss.getSheetByName("customer");
+  const data = ws.getRange("A1").getDataRegion().getValues();
+  const header = data.shift();
+
+  const jsonArray = data.map((r) => {
+    let obj = {};
+    header.forEach((h, i) => {
+      obj[h] = r[i];
+    });
+    return obj;
+  });
+
+  const response = [{ status: 200, data: jsonArray }];
+
   return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(
     ContentService.MimeType.JSON
   );
