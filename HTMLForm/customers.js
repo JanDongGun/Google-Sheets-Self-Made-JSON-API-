95eebbb62333079e18a8a40533f5ee82e87b2dbc
jandonggun <donggontrannguyen@gmail.com>
2021-03-10 10:47:32 +0700

    add spinner

--- HTMLForm/customers.js
+++ HTMLForm/customers.js
@@ -3,6 +3,9 @@ const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const phoneNumber = document.querySelector("#phoneNumber");
 const city = document.querySelector("#city");
+const submitBtn = document.querySelector("#submitBtn");
+const spinner = document.querySelector("#btnSpinner");
+const btnText = document.querySelector("#btnText");
 const url =
   "https://script.google.com/macros/s/AKfycbwGghfBaXwqYT8H-HZowqh2Sn8MDFJtiCDmbxaxU3zLH7ahzXxROJ1LKZn9A-c7pYyoeA/exec";
 
@@ -16,6 +19,10 @@ const afterSubmit = (e) => {
     city: city.value,
   };
 
+  btnText.textContent = "Saving ...";
+  spinner.classList.remove("d-none");
+  submitBtn.disabled = true;
+
   fetch(url, {
     method: "POST",
     cache: "no-cache",
@@ -23,13 +30,17 @@ const afterSubmit = (e) => {
     body: JSON.stringify(info),
   })
     .then((res) => res.json())
-    .then((res) => console.log(res))
+    .then((res) => {
+      console.log(res);
+      btnText.textContent = "Add";
+      spinner.classList.add("d-none");
+      submitBtn.disabled = false;
+      customerForm.reset();
+    })
     .catch((err) => {
       console.log(err);
       console.log("something went wrong!");
     });
-
-  customerForm.reset();
 };
 
 customerForm.addEventListener("submit", afterSubmit);
